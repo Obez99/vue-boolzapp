3d04918d1df86e5516eb107f0bab66cf60bd6504
Obez99 <giuliogx@live.it>
2021-11-04 15:38:28 +0100

    Adds dynamic date in messages

--- js/app.js
+++ js/app.js
@@ -10,16 +10,19 @@ const app = new Vue({
         date: "10/01/2020 16:15:22",
         messages: [
           {
+            messageDate: "10/01/2020 15:30:55",
             text: "Hai portato a spasso il cane?",
             status: "sent"
           },
 
           {
+            messageDate: "10/01/2020 15:50:00",
             text: "Ricordati di dargli da mangiare",
             status: "sent"
           },
 
           {
+            messageDate: "10/01/2020 16:15:22",
             text: "Tutto fatto?",
             status: "received"
           }
@@ -33,16 +36,19 @@ const app = new Vue({
         date: "10/01/2020 16:15:22",
         messages: [
           {
+            messageDate: "20/03/2020 16:30:00",
             text: "Ciao come stai?",
             status: "sent"
           },
 
           {
+            messageDate: "20/03/2020 16:30:55",
             text: "Bene grazie! Stasera ci vediamo?",
             status: "received"
           },
 
           {
+            messageDate: "20/03/2020 16:35:00",
             text: "Mi piacerebbe ma devo andare a fare la spesa.",
             status: "received"
           }
@@ -55,16 +61,19 @@ const app = new Vue({
         date: "10/01/2020 16:15:22",
         messages: [
           {
+            messageDate: "28/03/2020 10:10:40",
             text: "La Marianna va in campagna?",
             status: "received"
           },
 
           {
+            messageDate: "28/03/2020 10:20:10",
             text: "Sicuro di non aver sbagliato chat?",
             status: "sent"
           },
 
           {
+            messageDate: "28/03/2020 16:15:22",
             text: "Ah scusa!",
             status: "received"
           }
@@ -77,11 +86,13 @@ const app = new Vue({
         date: "10/01/2020 16:15:22",
         messages: [
           {
+            messageDate: "10/01/2020 15:30:55",
             text: "Lo sai che ha aperto una nuova pizzeria?",
             status: "sent"
           },
 
           {
+            messageDate: "10/01/2020 15:50:00",
             text: "Si, ma preferirei andare al cinema",
             status: "received"
           }
