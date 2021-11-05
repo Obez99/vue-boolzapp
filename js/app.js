c9ea85922935723d5b47e1499671f9fb5d3e664e
Obez99 <giuliogx@live.it>
2021-11-05 15:18:53 +0100

    Adds a button to send messages

--- js/app.js
+++ js/app.js
@@ -9,19 +9,19 @@ const app = new Vue({
         image: "img/avatar_1.jpg",
         messages: [
           {
-            messageDate: "10/01/2020 15:30:55",
+            date: "10/01/2020 15:30:55",
             text: "Hai portato a spasso il cane?",
             status: "sent"
           },
 
           {
-            messageDate: "10/01/2020 15:50:00",
+            date: "10/01/2020 15:50:00",
             text: "Ricordati di dargli da mangiare",
             status: "sent"
           },
 
           {
-            messageDate: "10/01/2020 16:15:22",
+            date: "10/01/2020 16:15:22",
             text: "Tutto fatto?",
             status: "received"
           }
@@ -34,19 +34,19 @@ const app = new Vue({
         image: "img/avatar_2.jpg",
         messages: [
           {
-            messageDate: "20/03/2020 16:30:00",
+            date: "20/03/2020 16:30:00",
             text: "Ciao come stai?",
             status: "sent"
           },
 
           {
-            messageDate: "20/03/2020 16:30:55",
+            date: "20/03/2020 16:30:55",
             text: "Bene grazie! Stasera ci vediamo?",
             status: "received"
           },
 
           {
-            messageDate: "20/03/2020 16:35:00",
+            date: "20/03/2020 16:35:00",
             text: "Mi piacerebbe ma devo andare a fare la spesa.",
             status: "received"
           }
@@ -58,19 +58,19 @@ const app = new Vue({
         image: "img/avatar_3.jpg",
         messages: [
           {
-            messageDate: "28/03/2020 10:10:40",
+            date: "28/03/2020 10:10:40",
             text: "La Marianna va in campagna?",
             status: "received"
           },
 
           {
-            messageDate: "28/03/2020 10:20:10",
+            date: "28/03/2020 10:20:10",
             text: "Sicuro di non aver sbagliato chat?",
             status: "sent"
           },
 
           {
-            messageDate: "28/03/2020 16:15:22",
+            date: "28/03/2020 16:15:22",
             text: "Ah scusa!",
             status: "received"
           }
@@ -82,13 +82,13 @@ const app = new Vue({
         image: "img/avatar_4.jpg",
         messages: [
           {
-            messageDate: "10/01/2020 15:30:55",
+            date: "10/01/2020 15:30:55",
             text: "Lo sai che ha aperto una nuova pizzeria?",
             status: "sent"
           },
 
           {
-            messageDate: "10/01/2020 15:50:00",
+            date: "10/01/2020 15:50:00",
             text: "Si, ma preferirei andare al cinema",
             status: "received"
           }
@@ -137,5 +137,9 @@ const app = new Vue({
         return contact.name.toLowerCase().includes(this.contactSearch.trim().toLowerCase())
       })
     },
+
+    getLastDate: function () {
+
+    }
   },
 })
